e53b1b1738a9f57b7afbd182c63b5f3856e2ee18
Akramsarker <dev.mdakram@gmail.com>
2021-08-09 05:01:49 +0600

    update some style add new style

---
+++ icon-menu.js
@@ -0,0 +1,10 @@
+
+// document.querySelector('i').addEventListener(("click", => {
+//   var x = document.getElementsByClassName("myLinks");
+//   if (list.style.display === "none") {
+//     list.style.display = "block";
+//   } else {
+//     // x.style.display = "block";
+//   }
+//   console.log('hello')
+// })
